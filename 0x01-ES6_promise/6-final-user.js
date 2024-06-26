3fcb55881e2d02d79b19616ab40e17fad7df32b9
Yosrahabouria <132350298+Yosrahabouria@users.noreply.github.com>
2024-06-26 20:01:43 +0200

    profile

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,15 +1,15 @@
-import signupUser from 4-user-promise.js;
-import uploadPhoto from 5-photo-reject.js;
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-    return Promise.allSettled([
-      signUpUser(firstName, lastName),
-      uploadPhoto(fileName),
-    ]).then((values) => {
-      const arr = [];
-      for (const item of values) {
-        arr.push({ status: item.status, value: item.value || item.reason });
-      }
-      return arr;
-    });
-  }
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]).then((values) => {
+    const arr = [];
+    for (const item of values) {
+      arr.push({ status: item.status, value: item.value || item.reason });
+    }
+    return arr;
+  });
+}
