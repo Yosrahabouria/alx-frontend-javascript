262037a1b6029edeed564879b57404ce44fba6ef
Yosrahabouria <yosrahabouria@gmail.com>
2024-06-28 09:34:41 -0700

    js

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,18 +1,13 @@
 import Building from './5-building';
-
 export default class SkyHighBuilding extends Building {
-  constructor(sqft, floors) {
-    super(sqft);
-    this._floors = floors;
-  }
-
-  // Getter for floors
-  get floors() {
-    return this._floors;
-  }
-
-  // override method
-  evacuationWarningMessage() {
-    return `Evacuate slowly the ${this.floors} floors`;
-  }
+	constructor(sqft, floors) {
+		super(sqft);
+		this._floors = floors;
+	}
+	get floors() {
+		return this._floors;
+	}
+	evacuationWarningMessage() {
+		return `Evacuate slowly the ${this.floors} floors`;
+	}
 }
