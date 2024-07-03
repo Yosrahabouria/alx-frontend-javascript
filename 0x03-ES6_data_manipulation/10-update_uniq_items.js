0debb935f3fbd78f67a5ce233efa6a20f7a77a95
Yosrahabouria <132350298+Yosrahabouria@users.noreply.github.com>
2024-07-03 14:15:24 +0200

    Update 10-update_uniq_items.js

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,7 +1,7 @@
 export default function updateUniqueItems(map) {
     if (!(map instanceof Map)) throw new Error('Cannot process');
     for (const [i, j] of map.entries()) {
-      if (i === 1) map.set(j, 100);
+        if (i === 1) map.set(i, 100); // Use `i` as the key and update its value
     }
     return map;
-  }
+}
